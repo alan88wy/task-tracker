35823207be85b3ea64e0c4c4c3cddab1d88c9294
Alan <alan88w@yahoo.com>
2022-04-15 22:02:51 +0800

    first commit

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,25 @@
+import { IcuPlaceholder } from '@angular/compiler/src/i18n/i18n_ast';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() text: string = "";
+  @Input() color: string = "";
+  @Output() btnClick = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick() {
+    this.btnClick.emit();
+  }
+
+}
